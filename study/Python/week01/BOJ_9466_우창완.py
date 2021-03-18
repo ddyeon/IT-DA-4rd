90ed4306c33ac1e27f3213d22133fc3fc45ee352
WooWan <woohobi@gmail.com>
2021-03-18 19:37:59 +0900

    Create HW 9466 우창완

--- study/Python/week01/BOJ_9466_우창완.py
+++ study/Python/week01/BOJ_9466_우창완.py
@@ -1,41 +1,78 @@
 import sys
-sys.setrecursionlimit(10000)
-n,m,h=map(int, input().split())
+from collections import deque
 
-milk=list()
-graph=list()
+t=int(input())
 
-for i in range(n):
-    graph.append(list(map(int,sys.stdin.readline().split())))
+def bfs(x):
+    queue=deque()
+    visited[x]=True
+    queue.append(x)
 
-answer=0
+    while queue:
+        x=queue.popleft()
+        #방문한적이 있다면,
+        nx=arr[x]
+        if visited[nx]:
+            if not finished[nx]:
+                # print(f' nx:{nx} x:{x}')
+                team[nx]=True
+                start=nx
+                nx=arr[nx]
+                while start!=nx:
+                    if nx == arr[nx]:
+                        team[nx] = True
+                        return
+                    # nx=arr[nx]
+                    team[nx]=True
+                    nx=arr[nx]
+        else:
+            visited[nx]=True
+            queue.append(nx)
+for _ in range(t):
+    n=int(sys.stdin.readline())
+    visited=[False]*(n+1)
+    team=[False]*(n+1)
+    finished=[False]*(n+1)
+    arr=[0]+list(map(int, sys.stdin.readline().split()))
+    for i in range(1,n+1):
+        if i==arr[i]:
+            team[i]=True
+            continue
+        if not visited[i]:
+            # visited=[False]*(n+1)
+            bfs(i)
+    print(team)
+    print(n-team.count(True))
 
-
-def backtracking(x, y,health,cnt):
-    global answer
-    # 집으로 돌아갈수 있다면 answer과 비교
-
-    for mx,my in milk:
-        if graph[mx][my]==2:
-            dist = abs(mx - x) + abs(my - y)
-            if dist<=health:
-                graph[mx][my] = 0
-                backtracking(mx,my,health+h-dist,cnt+1)
-                graph[mx][my]=2
-
-    if abs(hx - x) + abs(hy - y) <= health:
-        answer = max(answer, cnt)
-
-
-for i in range(n):
-    for j in range(n):
-        if graph[i][j]==1:
-            hx,hy= i,j
-        elif graph[i][j]==2:
-            milk.append((i,j))
-
-backtracking(hx,hy,m,0)
-print(answer)
-
-
-
+#bfs를 통해서 구현해보려고 했으나 실패
+#
+# import sys
+# sys.setrecursionlimit(100000)
+# t= int(input())
+#
+# def dfs(x):
+#     global cnt
+#     visited[x]=True
+#     nx=arr[x]
+#     if not visited[nx]: dfs(nx)
+#     else:
+#         if not finished[nx]:
+#             current= nx
+#             while current!=x:
+#                 cnt+=1
+#                 current=arr[current]
+#             cnt+=1
+#     finished[x]=True
+#
+#
+# for _ in range(t):
+#     n=int(input())
+#     arr=[0]+list(map(int, sys.stdin.readline().split()))
+#     visited= [False]*(n+1)
+#     finished=[False]*(n+1)
+#     cnt=0
+#
+#     for i in range(1,n+1):
+#         if not visited[i]:
+#             dfs(i)
+#     print(n-cnt)
