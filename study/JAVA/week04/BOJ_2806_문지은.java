741daeb08cc21feabfb1c701b94375aa8afd9818
moon jieun <55692557+s2moon98@users.noreply.github.com>
2021-04-08 18:48:49 +0900

    Create HW 2806 문지은

---
+++ study/JAVA/week04/BOJ_2806_문지은.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        String s;
+
+        n = sc.nextInt();
+        s = sc.next();
+
+        int[] dp = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++){
+            
+            
+
+        }
+
+    }
+}
