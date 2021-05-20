db7f49aaa86b7b4be53ae6e80e9c4c3ecc4e41a8
WooWan <woohobi@gmail.com>
2021-05-20 19:49:21 +0900

    Create STD 10476 우창완

---
+++ study/Python/week08/BOJ_2011_우창완.py
@@ -0,0 +1,15 @@
+#2011 암호코드
+#dp[n]=n번쨰 자리의 해석가능한 개수이고, i번째 자릿수가 0보다크다면 dp[i]+=dp[i-1]
+#s[i-1:i+1]이 10~26 사이라면 dp[i]+=dp[i-2]이다.
+
+s=str(input())
+if s[0]=="0":
+    print(0)
+else:
+    dp=[0]*5001
+    dp[0]=dp[1]=1
+    for i in range(1,len(s)):
+        if int(s[i])>0: dp[i]+=dp[i-1]
+        if int(s[i-1:i+1])>=10 and int(s[i-1:i+1])<=26:
+            dp[i]+=dp[i-2]
+    print(dp[len(s)-1]%1000000)
