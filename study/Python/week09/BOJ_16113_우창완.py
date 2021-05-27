4c28a680365a10165d39cf5b851e69f6708ee5fe
WooWan <woohobi@gmail.com>
2021-05-27 18:58:36 +0900

    Create HW 16113 우창완

--- study/Python/week09/BOJ_16113_우창완.py
+++ study/Python/week09/BOJ_16113_우창완.py
@@ -1,5 +1,6 @@
 #16113 시그널
-#생각: 1과 나머지 다른 숫자를 구분해야 될 것 같음.
+#생각: 1과 나머지 다른 숫자를 구분해야 될 것 같음..
+# #의 개수만으로는 판단 불가
 
 import sys
 input=sys.stdin.readline
@@ -9,5 +10,12 @@ arr=list(map(str,input().strip()))
 
 row=len(arr)//5
 print(row)
+result=[[]*row for i in range(5)]
+print(result)
+for i in range(5):
+    result[i].append(arr[i*row:i*row+row])
 
-for i in range(row):
+for j in range(row):
+    for i in range(5):
+        if result[i][j]=="#":
+            if result[i+1][j]=="#"=="#"
