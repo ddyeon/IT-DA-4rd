bd54db8467a8db9d34fdca3eae43a58e07f23e8b
Kangchaemin <43837994+Kangchaemin@users.noreply.github.com>
2021-06-04 21:43:15 +0900

    Update STD 2608 강채민

--- study/JAVA/week09/BOJ_2608_강채민.java
+++ study/JAVA/week09/BOJ_2608_강채민.java
@@ -1,9 +1,12 @@
 import java.util.*;
 
-public class 2608_로마숫자 {
+public class 2608_ {
 	
 	/**
-	 * 아직 못품 ㅜㅜ
+	 * https://github.com/ddyeon/IT-DA-4rd/blob/main/study/JAVA/week09/BOJ_2608_%EA%B9%80%EC%98%81%EB%AF%BC.java
+	 * 팀장님 코드랑 팀원들 코드 보면서 참고했습니다
+	 * 
+	 * 엄청나게 복잡할것처럼 보였는데... 그냥 구현하면 되는거였다니..?
 	 */
 
 	public static void main(String[] args) {
@@ -23,29 +26,142 @@ public class 2608_로마숫자 {
 		    put("M", 1000);
 		}};
 		
-		int beforeInt = map.get(Character.toString(S1.charAt(0)));
+		int sum1 = 0;
 		
-		int sum1 = beforeInt;
-		
-		for(int i = 1; i < S1.length(); i++) {
+		for(int i = 0; i < S1.length(); i++) {
 			
 			String nowS = Character.toString(S1.charAt(i));
 			int nowInt = map.get(nowS);
 			
-			if(beforeInt < nowInt) 
-				sum1 += nowInt - beforeInt;
+			if(i + 1 >= S1.length()) {
+				
+				sum1 += nowInt;
+				continue;
+			}
 			
-			else {
-				sum1 += beforeInt;
-				beforeInt = nowInt;
+			String nextS = Character.toString(S1.charAt(i + 1));
+			int nextInt = map.get(nextS);
+			
+			if(nowInt < nextInt) {
+				
+				sum1 += nextInt - nowInt;
+				i++;
+			}
+		
+			else 
+				sum1 += nowInt;
+			
+		}
+		
+		int sum2 = 0;
+		
+		for(int i = 0; i < S2.length(); i++) {
+			
+			String nowS = Character.toString(S2.charAt(i));
+			int nowInt = map.get(nowS);
+			
+			if(i + 1 >= S2.length()) {
+				
+				sum2 += nowInt;
+				continue;
+			}
+			
+			String nextS = Character.toString(S2.charAt(i + 1));
+			int nextInt = map.get(nextS);
+			
+			if(nowInt < nextInt) {
+				
+				sum2 += nextInt - nowInt;
+				i++;
+			}
+		
+			else 
+				sum2 += nowInt;
+			
+		}
+		
+		System.out.println(sum1 + sum2);
+		
+		String answer = "";
+		int num = sum1 + sum2;
+		
+		/**
+		 * 여기서부터가 약간 ..
+		 * 
+		 * while(num >= 0)을 하면 안된다
+		 */
+		
+		while(num != 0) {
+			
+			if(num >= 1000) {
+				answer += "M";
+				num -= 1000;
+			}
+			
+			else if(num >= 900) {
+				answer += "CM";
+				num -= 900;
+			}
+			
+			else if(num >= 500) {
+				answer += "D";
+				num -= 500;
+			}
+			
+			else if(num >= 400) {
+				answer += "CD";
+				num -= 400;
+			}
+			
+			else if(num >= 100) {
+				answer += "C";
+				num -= 100;
+			}
+			
+			else if(num >= 90) {
+				answer += "XC";
+				num -= 90;
+			}
+			
+			else if(num >= 50) {
+				answer += "L";
+				num -= 50;
+			}
+			
+			else if(num >= 40) {
+				answer += "XL";
+				num -= 40;
+			}
+			
+			else if(num >= 10) {
+				answer += "X";
+				num -= 10;
+			}
+			
+			else if(num >= 9) {
+				answer += "IX";
+				num -= 9;
+			}
+			
+			else if(num >= 5) {
+				answer += "V";
+				num -= 5;
+			}
+			
+			else if(num >= 4) {
+				answer += "IV";
+				num -= 4;
+			}
+			
+			else if(num >= 1) {
+				answer += "I";
+				num -= 1;
 			}
 			
 		}
 		
-		//마지막수 더해주기?
-		sum1 += map.get(Character.toString(S1.charAt(S1.length() - 1)));
+		System.out.println(answer);
 		
-		System.out.println(sum1);
 
 	}
 
